4ccaaf65eef6d54d96cf0b09bca7117ac1ecd44b
KomalMete <komalmete8@gmail.com>
2023-07-25 10:40:05 +0530

    this is first armstrong number

---
+++ Armstrong.java
@@ -0,0 +1,35 @@
+package sdm;
+
+
+	import java.util.Scanner;
+	public class Armstrong {
+
+		public static void main(String[] args) {
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Enter the number : ");
+			int num = sc.nextInt();
+					int ref = 0;
+					double sum = 0;
+					int trav = num; 
+			while( num > 0)
+			{
+				ref = num % 10;
+				sum = sum + Math.pow(ref, 3);
+				num = num/10;
+			}
+			System.out.println("sum of cubes of digits in number : "+sum);
+			
+			if(sum == trav)
+			{
+				System.out.println(sum + " is a Armstrong Number ");
+			}
+			else
+			{
+				System.out.println(sum + " is not a Armstrong number ");
+			}
+
+		}
+
+	}
+
+
